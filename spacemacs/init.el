df9c9fefa6f8f6b0d8ab6b768620e06022c36957
Lee Henson <lee.m.henson@gmail.com>
2016-05-30 20:15:46 +0100

    added ruby layer

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -31,6 +31,7 @@ values."
      ;; org
      osx
      react
+     (ruby :variables ruby-version-manager 'chruby)
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
