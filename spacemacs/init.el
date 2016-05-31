9cc3d9521da8307e51539e63bd6bc921ccb1993a
Lee Henson <lee.m.henson@gmail.com>
2016-05-31 11:07:00 +0100

    added global line numbers setting

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -215,6 +215,9 @@ values."
    ;; scrolling overrides the default behavior of Emacs which recenters the
    ;; point when it reaches the top or bottom of the screen. (default t)
    dotspacemacs-smooth-scrolling t
+   ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'.
+   ;; (default nil)
+   dotspacemacs-global-line-numbers t
    ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
    ;; derivatives. If set to `relative', also turns on relative line numbers.
    ;; (default nil)
