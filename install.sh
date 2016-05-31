4e0e3b5877d092a499286e254aa8a8d8ef9d0467
Lee Henson <lee.m.henson@gmail.com>
2016-05-31 13:32:50 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-ln -sf "$DOTFILES/jsbeautifier/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
+ln -sf "$DOTFILES/js-beautify/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
 ln -sf "$DOTFILES/tern/.tern-project" "$HOME/.tern-project"
