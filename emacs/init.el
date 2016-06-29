ef95f23ea5d272d21f3233d0c83e9c31bddc885a
Lee Henson <lee.m.henson@gmail.com>
2016-06-29 14:34:40 +0100

    reintroduce exec-path-from-shell

--- emacs/init.el
+++ emacs/init.el
@@ -118,12 +118,13 @@
   (setq-default dired-details-hidden-string "")
   (dired-details-install))
 
-;; (use-package exec-path-from-shell
-;;   :ensure t
-;;   :init
-;;   (setq exec-path-from-shell-check-startup-files nil)
-;;   :config
-;;   (exec-path-from-shell-initialize))
+(use-package exec-path-from-shell
+  :ensure t
+  :init
+  (setq exec-path-from-shell-arguments '("-c"))
+  (setq exec-path-from-shell-check-startup-files nil)
+  :config
+  (exec-path-from-shell-initialize))
 
 (use-package flycheck
   :ensure t
