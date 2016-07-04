bce85778d6e3b1382f111c26062417cfb4cfe348
Lee Henson <lee.m.henson@gmail.com>
2016-07-04 13:17:19 +0100

    switch theme

--- emacs/init.el
+++ emacs/init.el
@@ -38,7 +38,7 @@
 ;; Theming.
 (use-package base16-theme
   :ensure t)
-(load-theme 'base16-twilight-dark t)
+(load-theme 'base16-tomorrow-dark t)
 (global-hl-line-mode +1)
 
 ;; (use-package planet-theme
@@ -268,7 +268,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d1a42ed39a15a843cccadf107ee0242b5f78bfbb5b70ba3ce19f3ea9fda8f52d" "6d8d8af130783e4e91bfcda284ad3271e598e9e78374f5ad406ab63243e75982" "2a5be663818e1e23fd2175cc8dac8a2015dcde6b2e07536712451b14658bbf68" "b869a1353d39ab81b19eb79de40ff3e7bb6eaad705e61f7e4dbdcb183f08c5a6" "e87a2bd5abc8448f8676365692e908b709b93f2d3869c42a4371223aab7d9cf8" default)))
+    ("b6db49cec08652adf1ff2341ce32c7303be313b0de38c621676122f255ee46db" "d1a42ed39a15a843cccadf107ee0242b5f78bfbb5b70ba3ce19f3ea9fda8f52d" "6d8d8af130783e4e91bfcda284ad3271e598e9e78374f5ad406ab63243e75982" "2a5be663818e1e23fd2175cc8dac8a2015dcde6b2e07536712451b14658bbf68" "b869a1353d39ab81b19eb79de40ff3e7bb6eaad705e61f7e4dbdcb183f08c5a6" "e87a2bd5abc8448f8676365692e908b709b93f2d3869c42a4371223aab7d9cf8" default)))
  '(package-selected-packages
    (quote
     (window-numbering spaceline smooth-scrolling pbcopy magit json-mode js2-mode hl-todo highlight-parentheses flycheck exec-path-from-shell dired-details desktop+ counsel avy auto-complete ag evil-surround evil-nerd-commenter evil-leader evil base16-theme use-package))))
