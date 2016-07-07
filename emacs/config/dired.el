0f79f81444e80cacecec2e54ba22833b613c09d2
Lee Henson <lee.m.henson@gmail.com>
2016-07-07 20:09:34 +0100

    better dired config

--- emacs/config/dired.el
+++ emacs/config/dired.el
@@ -1,8 +1,15 @@
-(use-package dired-details
+(use-package dired-single
   :ensure t
   :config
-  (setq-default dired-details-hidden-string "")
-  (dired-details-install))
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map "-"
+    (function
+     (lambda nil (interactive) (dired-single-buffer "..")))))
+
+(use-package diredful
+  :ensure t
+  :config
+  (diredful-mode))
 
 (define-key evil-normal-state-map (kbd "-") 'dired-jump)
 (define-key dired-mode-map "c" 'find-file)
