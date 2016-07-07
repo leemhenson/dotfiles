b9f3b73084b0b49021747ff91eca180321112680
Lee Henson <lee.m.henson@gmail.com>
2016-07-07 20:44:13 +0100

    fix error messages from theme var usage

--- emacs/config/theme.el
+++ emacs/config/theme.el
@@ -15,9 +15,9 @@
   :ensure t
   :config
   (load-theme 'planet t)
-  (setq hl-inward-paren-bg-color 'planet-bg)
+  (setq hl-inward-paren-bg-color "#192129")
   (setq hl-inward-paren-fg-color "LimeGreen")
-  (setq hl-outward-paren-bg-colors '(planet-bg))
+  (setq hl-outward-paren-bg-colors '("#192129"))
   (setq hl-outward-paren-fg-colors '("gold"
                                      "chocolate"
                                      "SteelBlue1"
