3eb983343213c8390558cb2cb55cfccdc7518bcd
Lee Henson <lee.m.henson@gmail.com>
2016-07-08 20:10:10 +0100

    fix kill-other-buffers

--- emacs/config/buffers.el
+++ emacs/config/buffers.el
@@ -5,12 +5,18 @@
   (global-anzu-mode))
 
 (defun kill-other-buffers ()
-  "Kill all buffers but the current one. Doesn't mess with special buffers."
+  "Kill all other buffers."
   (interactive)
   (when (y-or-n-p "Are you sure you want to kill all buffers but the current one? ")
-    (seq-each
-     #'kill-buffer
-     (delete (current-buffer) (seq-filter #'buffer-file-name (buffer-list))))))
+    (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))))
+
+;; (defun kill-other-buffers ()
+;;   "Kill all buffers but the current one. Doesn't mess with special buffers."
+;;   (interactive)
+;;   (when (y-or-n-p "Are you sure you want to kill all buffers but the current one? ")
+;;     (seq-each
+;;      #'kill-buffer
+;;      (delete (current-buffer) (seq-filter #'buffer-file-name (buffer-list))))))
 
 (global-set-key (kbd "C-h") 'windmove-left)
 (global-set-key (kbd "C-l") 'windmove-right)
