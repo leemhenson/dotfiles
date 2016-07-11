b27682d9f30b665514a47678c6a58b64522e8caa
Lee Henson <lee.m.henson@gmail.com>
2016-07-11 20:18:14 +0100

    use noctilux theme

--- emacs/config/highlighting.el
+++ emacs/config/highlighting.el
@@ -2,7 +2,15 @@
   :ensure t
   :diminish 'hl-paren-mode
   :config
-  (add-hook 'prog-mode-hook #'hl-paren-mode))
+  (add-hook 'prog-mode-hook #'hl-paren-mode)
+  (setq hl-inward-paren-bg-color "#192129")
+  (setq hl-inward-paren-fg-color "LimeGreen")
+  (setq hl-outward-paren-bg-colors '("#192129"))
+  (setq hl-outward-paren-fg-colors '("gold"
+                                     "chocolate"
+                                     "SteelBlue1"
+                                     "MistyRose"
+                                     "MediumPurple")))
 
 (use-package hl-todo
   :ensure t
--- emacs/config/theme.el
+++ emacs/config/theme.el
@@ -1,29 +1,10 @@
-;; Theming.
-;; (use-package base16-theme
-;;   :ensure t)
-
-;; (use-package gruber-darker-theme
-;;   :ensure t)
-
-;; (use-package noctilux-theme
-;;   :ensure t)
-
-;; (use-package soothe-theme
-;;   :ensure t)
-
 (use-package planet-theme
+  :ensure t)
+
+(use-package noctilux-theme
   :ensure t
   :config
-  (load-theme 'planet t)
-  (setq hl-inward-paren-bg-color "#192129")
-  (setq hl-inward-paren-fg-color "LimeGreen")
-  (setq hl-outward-paren-bg-colors '("#192129"))
-  (setq hl-outward-paren-fg-colors '("gold"
-                                     "chocolate"
-                                     "SteelBlue1"
-                                     "MistyRose"
-                                     "MediumPurple"))
-  )
+  (load-theme 'noctilux t))
 
 (set-face-attribute 'default nil
                     :height 140
