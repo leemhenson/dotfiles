ffcae758d52430be0dfd6c60527ca62f1d1d8ce6
Lee Henson <lee.m.henson@gmail.com>
2016-07-11 09:19:10 +0100

    install.sh needs to symlink gitconfig for magit

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+ln -sf "$DOTFILES/git/config" "$HOME/.gitconfig"
 ln -sf "$DOTFILES/js-beautify/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
 ln -sf "$DOTFILES/tern/.tern-project" "$HOME/.tern-project"
