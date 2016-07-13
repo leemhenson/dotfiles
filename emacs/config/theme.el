18ff0a935f49894acbb39f22f9ea83aa722680b7
Lee Henson <lee.m.henson@gmail.com>
2016-07-13 14:18:41 +0100

    back to planet theme

--- emacs/config/theme.el
+++ emacs/config/theme.el
@@ -1,11 +1,6 @@
 (use-package planet-theme
   :ensure t)
 
-(use-package noctilux-theme
-  :ensure t
-  :config
-  (load-theme 'noctilux t))
-
 (set-face-attribute 'default nil
                     :height 120
                     :family "InconsolataDzfp")
@@ -13,5 +8,5 @@
 (use-package smart-mode-line
   :ensure t
   :config
-  (sml/setup)
-  (load-theme 'smart-mode-line-respectful t))
+  (setq sml/theme 'dark)
+  (sml/setup))
