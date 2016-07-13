ba7068bc24960e4fa1a82342438f97ecc188c42e
Lee Henson <lee.m.henson@gmail.com>
2016-07-13 21:27:23 +0100

    more theme diving

--- emacs/config/theme.el
+++ emacs/config/theme.el
@@ -1,5 +1,23 @@
-(use-package planet-theme
+;; (use-package subatomic-theme
+;;   :ensure t)
+
+;; (use-package sublime-themes
+;;   :ensure t)
+
+;; (use-package flatland-theme
+;;   :ensure t)
+
+;; (use-package gruber-darker-theme
+;;   :ensure t)
+
+(use-package ujelly-theme
   :ensure t)
+
+;; (use-package afternoon-theme
+;;   :ensure t)
+
+;; (use-package planet-theme
+;;   :ensure t)
 
 (set-face-attribute 'default nil
                     :height 120
