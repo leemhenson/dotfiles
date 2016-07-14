bc1a6068a06cebd9e967f4ea413598dcf3778afd
Lee Henson <lee.m.henson@gmail.com>
2016-07-14 16:44:43 +0100

    enable yas-global-mode

--- emacs/config/yasnippet.el
+++ emacs/config/yasnippet.el
@@ -1,9 +1,6 @@
 (use-package yasnippet
   :ensure t
-  ;; :commands yas-minor-mode
-  ;; :diminish yas-minor-mode
-  ;; :init
-  ;; (progn (add-hook 'prog-mode-hook #'yas-minor-mode))
   :config
   (setq yas-snippet-dirs '("/Users/leemhenson/.dotfiles/emacs/snippets"))
-  (progn (yas-reload-all)))
+  (progn (yas-reload-all))
+  (yas-global-mode))
