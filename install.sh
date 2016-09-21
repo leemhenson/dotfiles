304d6722166681cc6ecf4882ae56fb4f8f7d6b8c
Lee Henson <lee.m.henson@gmail.com>
2016-09-21 21:59:50 +0100

    install ghci-color

--- install.sh
+++ install.sh
@@ -4,3 +4,13 @@ ln -sf "$DOTFILES/emacs/daemon.plist" "$HOME/Library/LaunchAgents/emacs-deamon.p
 ln -sf "$DOTFILES/git/config" "$HOME/.gitconfig"
 ln -sf "$DOTFILES/js-beautify/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
 ln -sf "$DOTFILES/tern/.tern-project" "$HOME/.tern-project"
+
+other="$HOME/src/other"
+ghci_color="$other/ghci-color"
+
+if [ ! -d $ghci_color ]; then
+  mkdir -p $other
+  git clone https://github.com/rhysd/ghci-color.git $ghci_color
+  echo "${ghci_color}/ghci-color"
+  ln -sf "${ghci_color}/ghci-color" /usr/local/bin/ghci-color
+fi
