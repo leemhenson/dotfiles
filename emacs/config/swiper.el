596ae01cdf1e724e52840eb128a8d6e2b6e74a63
Lee Henson <lee.m.henson@gmail.com>
2016-10-07 15:54:00 +0100

    reinstate projectile-ag

--- emacs/config/swiper.el
+++ emacs/config/swiper.el
@@ -13,7 +13,8 @@
   (projectile-global-mode)
   (setq projectile-completion-system 'ivy)
   (evil-leader/set-key
-    "pd" 'projectile-dired))
+    "pd" 'projectile-dired
+    "pa" 'projectile-ag))
 
 (use-package counsel
   :ensure t
@@ -26,6 +27,13 @@
              counsel-M-x
              swiper)
   :init
+  (defun counsel-ag-advice (args)
+    "Make counsel-ag aware of project root directory,
+modify INITIAL-DIRECTORY to projectile-project-root."
+    (setcar (nthcdr 1 args) projectile-project-root))
+
+  (advice-add 'counsel-ag :filter-args #'counsel-ag-advice)
+
   (evil-leader/set-key
     "ca" 'counsel-ag
     "cb" 'ivy-switch-buffer
@@ -43,4 +51,3 @@
   :commands counsel-projectile
   :init
   (evil-leader/set-key "cp" 'counsel-projectile-switch-project))
-
