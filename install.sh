9bccdd53569d6e51de4a827bbe78d29e32f18c63
Lee Henson <lee.m.henson@gmail.com>
2016-11-22 16:52:24 +0000

    feat: ln emacs.d in $HOME

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+ln -sf "$DOTFILES/emacs" "$HOME/.emacs.d"
 ln -sf "$DOTFILES/emacs/daemon.plist" "$HOME/Library/LaunchAgents/emacs-deamon.plist"
 ln -sf "$DOTFILES/git/config" "$HOME/.gitconfig"
 ln -sf "$DOTFILES/js-beautify/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
