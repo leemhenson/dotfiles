b64883e2af8583bc0fb091c2ef7a30d63b68fbb5
Lee Henson <lee.m.henson@gmail.com>
2016-11-23 08:49:30 +0000

    feat: change cursor for vi modes in iterm

--- zsh/plugins/vi-mode/vi-mode.zsh
+++ zsh/plugins/vi-mode/vi-mode.zsh
@@ -1,15 +1,29 @@
 # Updates editor information when the terminal start or keymap changes.
-function zle-line-init zle-keymap-select() {
-  zle reset-prompt
-  zle -R
+function zle-keymap-select zle-line-init
+{
+    # change cursor shape in iTerm2
+    case $KEYMAP in
+        vicmd)      print -n -- "\E]50;CursorShape=0\C-G";;  # block cursor
+        viins|main) print -n -- "\E]50;CursorShape=1\C-G";;  # line cursor
+    esac
+
+    zle reset-prompt
+    zle -R
 }
+
+function zle-line-finish
+{
+    print -n -- "\E]50;CursorShape=0\C-G"  # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
 
 # Ensure that the prompt is redrawn when the terminal size changes.
 TRAPWINCH() {
   zle && { zle reset-prompt; zle -R }
 }
-
-zle -N zle-keymap-select
 
 bindkey -v
 bindkey '^h' backward-delete-char
