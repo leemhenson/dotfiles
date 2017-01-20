633e779a8ce3d507ecc02637f5089304ddc5f862
Lee Henson <lee.m.henson@gmail.com>
2017-01-20 13:29:33 +0000

    feat: make flog full height

--- zsh/plugins/fzf/fzf.zsh
+++ zsh/plugins/fzf/fzf.zsh
@@ -33,6 +33,7 @@ flog() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --height 100% \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
