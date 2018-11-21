5bf9c2ab2475d8bcf8980a3c0303d42c5cdf712f
Lee Henson <lee.m.henson@gmail.com>
2018-11-21 14:55:12 +0000

    home-manager: added git

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -5,6 +5,93 @@
     pkgs.htop
   ];
 
+  programs.git = {
+    aliases = {
+      tree = "log --all --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset' --abbrev-commit --date-order";
+    };
+    enable = true;
+    extraConfig = ''
+      [apply]
+      whitespace = nowarn
+
+      [color]
+      diff = auto
+      status = auto
+      branch = auto
+      ui = true
+      interactive = auto
+
+      [color "diff-highlight"]
+      oldNormal = red bold
+      oldHighlight = red bold 52
+      newNormal = green bold
+      newHighlight = green bold 22
+
+      [core]
+      editor = code --wait
+      excludesfile = $XDG_CONFIG_HOME/git/ignore
+
+      [diff]
+      tool = Kaleidoscope
+
+      [difftool]
+      prompt = false
+
+      [difftool "Kaleidoscope"]
+      cmd = ksdiff --partial-changeset --relative-path \"$MERGED\" -- \"$LOCAL\" \"$REMOTE\"
+
+      [fetch]
+      prune = true
+
+      [help]
+      autocorrect = 1
+
+      [merge]
+      tool = vscode
+
+      [mergetool]
+      keepBackup = false
+
+      [mergetool "Kaleidoscope"]
+      cmd = ksdiff --merge --output \"$MERGED\" --base \"$BASE\" -- \"$LOCAL\" --snapshot \"$REMOTE\" --snapshot
+      trustExitCode = true
+
+      [mergetool "vscode"]
+      cmd = code --wait $MERGED
+
+      [pager]
+      diff = diff-so-fancy | less --tabs=2 -RFX
+      show = diff-so-fancy | less --tabs=4 -RFX
+
+      [pull]
+      rebase = true
+
+      [push]
+      default = current
+    '';
+    ignores = [
+      "*~"
+      ''\#*\#''
+      ''\.#*''
+      ".DS_Store"
+      "/.dir-locals.el"
+      "/.envrc"
+      "/.vscode"
+      "/npm-debug.log*"
+      "/tags"
+      "/tags.lock"
+      "/tags.temp"
+      "/vendor"
+      "/yarn-error.log"
+    ];
+    signing = {
+      key = "B1EA4611F4564B0C487DF4B44CC045383A6DCF55";
+      signByDefault = true;
+    };
+    userEmail = "lee.m.henson@gmail.com";
+    userName = "Lee Henson";
+  };
+
   programs.home-manager = {
     enable = true;
     path = https://github.com/rycee/home-manager/archive/master.tar.gz;
