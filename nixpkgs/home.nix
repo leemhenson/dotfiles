706da787e9a6cbbae7ed3d36b9edad8468905b79
Lee Henson <lee.m.henson@gmail.com>
2018-11-22 12:42:35 +0000

    home-manager: zsh

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,9 +1,48 @@
 { pkgs, ... }:
 
 {
+  home.file.".zshenv".text = ''
+    export LANG=en_GB.UTF-8
+    export LC_ALL=en_GB.UTF-8
+  '';
+
+  home.file.".direnvrc".text = ''
+    use_ruby() {
+      local ver=$1
+
+      if [[ -z $ver ]] && [[ -f .ruby-version ]]; then
+        ver=$(cat .ruby-version)
+      fi
+
+      if [[ ! -z $ver ]]; then
+        chruby $ver
+      fi
+    }
+  '';
+
   home.packages = [
-    pkgs.htop
+    pkgs.bat
+    pkgs.cheat
+    pkgs.chruby
+    pkgs.httpie
   ];
+
+  programs.bash = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    defaultOptions = [ "-e" "--height 25%" "--reverse" ];
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
 
   programs.git = {
     aliases = {
@@ -95,5 +134,71 @@
   programs.home-manager = {
     enable = true;
     path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+  };
+
+  programs.htop = {
+    enable = true;
+  };
+
+  programs.man = {
+    enable = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.ssh = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    history = {
+      expireDuplicatesFirst = true;
+      extended = true;
+      save = 100000;
+      size = 100000;
+    };
+    initExtra = ''
+      KEYTIMEOUT=1
+
+      # Do not require a leading '.' in a filename to be matched explicitly
+      setopt GLOBDOTS
+      # Shaddapayourface
+      unsetopt BEEP
+
+      # Remove command lines from the history list when the first character on the
+      # line is a space, or when one of the expanded aliases contains a leading space
+      setopt HIST_IGNORE_SPACE
+      # Remove superfluous blanks from each command line being added to the history list
+      setopt HIST_REDUCE_BLANKS
+      # immediately appends new commands to the histfile
+      setopt INC_APPEND_HISTORY
+
+      source $HOME/.dotfiles/oh-my-zsh/plugins/vi-mode.zsh
+      source $HOME/.nix-profile/share/chruby/chruby.sh
+    '';
+    oh-my-zsh = {
+      custom = "$DOTFILES/oh-my-zsh";
+      enable = true;
+      plugins = [ "git" "httpie" "stack" "vi-mode" "z" ];
+      theme = "custom";
+    };
+    sessionVariables = {
+      CHEATCOLORS = true;
+      CLICOLOR = true;
+      DOTFILES = "$HOME/.dotfiles";
+      DEFAULT_CHEAT_DIR = "$DOTFILES/cheatsheets";
+    };
+    shellAliases = {
+      grom = "git rebase origin/master";
+      gt = "git tree";
+      j = "z";
+      ls = "ls -alh";
+    };
   };
 }
--- zsh/plugins/editcommand/editcommand.zsh
+++
@@ -1,5 +0,0 @@
-# allow <C-e> to edit the command line (standard behaviour)
-autoload -Uz edit-command-line
-zle -N edit-command-line
-bindkey '^e' edit-command-line
-
